424fe376f91e0342f3ef1c0460c4610f2b5a3621
David Cabrera <dcabrera1982@gmail.com>
2019-07-19 21:04:50 -0400

    linked api.giphy.com

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,1 +1,23 @@
+$("button").on("click", function() {
+    var sport = $(this).attr("data-person");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        sport + "&api_key=TF3a14L9272sA655laD1aQjwrZL8sFVw&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function(response) {
+            var results = response.data;
+        
+        for (var i = 0; i < results.length; i++) {
+
+            }
+        })
+})
+
+
+
 var topics = ["crossfit", "baseball", "basketball", "football"];
+console.log(topics);
+
