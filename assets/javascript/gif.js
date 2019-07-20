6b8eb0bf4d045249b3971c41e9936b84d61b04d1
David Cabrera <dcabrera1982@gmail.com>
2019-07-19 22:28:07 -0400

    gifs linked and buttons added

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,23 +1,25 @@
 $("button").on("click", function() {
-    var sport = $(this).attr("data-person");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        sport + "&api_key=TF3a14L9272sA655laD1aQjwrZL8sFVw&limit=10";
+    var sport = $(this).attr("data-sport");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=TF3a14L9272sA655laD1aQjwrZL8sFVw&limit=10"
 
     $.ajax({
         url: queryURL,
         method: "GET"
-    })
-        .then(function(response) {
-            var results = response.data;
-        
+    }).then(function(response) {
+        console.log(response);
+
+
+        var rating = response.Rated;
+        var results = response.data;
+
         for (var i = 0; i < results.length; i++) {
 
-            }
-        })
-})
-
-
-
-var topics = ["crossfit", "baseball", "basketball", "football"];
-console.log(topics);
-
+            var sportDiv = $("<div>");
+            var p = $("<p>").text(results[i].rating);
+            var sportImage = $("<img>").attr("src", results[i].images.fixed_height.url);
+            sportDiv.append(p);
+            sportDiv.append(sportImage);
+            $("#gifs-appear-here").prepend(sportDiv);
+        }
+    });
+});
