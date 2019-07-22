322c0f042ca5e040e76d2934ac5471131cbb2e73
David Cabrera <dcabrera1982@gmail.com>
2019-07-21 20:46:55 -0400

    add start/stop to gifs

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,25 +1,80 @@
-$("button").on("click", function() {
-    var sport = $(this).attr("data-sport");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=TF3a14L9272sA655laD1aQjwrZL8sFVw&limit=10"
+// Initial Search Buttons
+var topics = ["crossfit", "basketball", "football", "baseball", "soccer"];
+function addSearchBtns() {
+  $("#buttons").html("");
+  for (i = 0; i < topics.length; i++) {
+    var $button = $("<input type='button' class='btn btn-sm search-btn' />");
+    $button.val(topics[i]);
+    $("#buttons").append($button);
+  }
+}
+addSearchBtns();
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
+$(document).on("click", ".btn", function() {
+  $("#results").html("");
+  // Beginning API call
+  var queryURL = "https://api.giphy.com/v1/gifs/search?";
+  var query;
+  var params = {
+    q: query,
+    limit: 10,
+    api_key: "TF3a14L9272sA655laD1aQjwrZL8sFVw",
+    fmt: "json"
+  };
+  if ($(this).hasClass("search-btn")) {
+    query = $(this).val();
+  } else if ($("#user-search").val() !== "") {
+    query = $("#user-search").val();
+    topics.push(query);
+    if (topics.length > 6) {
+      topics.shift();
+    }
+    addSearchBtns();
+  }
+  params.q = query;
 
+  if ($(this).hasClass("sport")) {
+    queryURL = "https://api.giphy.com/v1/gifs/search?";
+    delete params.q;
+  }
+  $.ajax({
+    url: queryURL + $.param(params),
+    method: "GET",
+    success: function(r) {
+      for (i = 0; i < params.limit; i++) {
+        var $img = $("<img>");
+        var $div = $("<div>");
+        var $rating = $("<h6>");
+        var gifObj = r.data[i];
+        var gif = gifObj.images;
 
-        var rating = response.Rated;
-        var results = response.data;
+        // Image builder object
+        $img.attr({
+          // "width": "200px",
+          src: gif.fixed_height_still.url,
+          "data-animate": gif.fixed_height.url,
+          "data-still": gif.fixed_height_still.url,
+          "data-state": "still",
+          class: "gif"
+        });
+        // $div.attr("id", "gif-" + i);
+        $div.addClass("gif-box");
+        $rating.text("Rating: " + gifObj.rating);
+        $div.append($img, $rating);
+        $("#results").append($div);
+      }
 
-        for (var i = 0; i < results.length; i++) {
-
-            var sportDiv = $("<div>");
-            var p = $("<p>").text(results[i].rating);
-            var sportImage = $("<img>").attr("src", results[i].images.fixed_height.url);
-            sportDiv.append(p);
-            sportDiv.append(sportImage);
-            $("#gifs-appear-here").prepend(sportDiv);
+      $(".gif").on("click", function() {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
         }
-    });
+      });
+    }
+  });
 });
+
